23813ab4543e1b10e91f40530659ce01358525aa
Alexander R. Torrijos <artorrijos@gmail.com>
2017-08-05 14:02:41 +0000

    Updates

--- app/cron.php
+++ app/cron.php
@@ -4,6 +4,11 @@ namespace App;
 
 use GuzzleHttp\Client;
 
+require_once( dirname(__DIR__) . '/../../../wp-admin/includes/media.php' );
+require_once( dirname(__DIR__) . '/../../../wp-admin/includes/file.php' );
+require_once( dirname(__DIR__) . '/../../../wp-admin/includes/media.php' );
+
+
 add_action('movie_get_new_movies', function () {
     
     $client = new Client(['base_uri' => 'http://www.rcksld.com/']);
@@ -28,35 +33,12 @@ add_action('movie_get_new_movies', function () {
         // Add Custom Fields
         \add_post_meta($post_id, 'Director', $movie->Director);
         \add_post_meta($post_id, 'MainCast', $movie->MainCast);
+    
+        \error_log('media_sideload_image: ' . (function_exists('media_sideload_image') ? 'true' : 'false'));    
         
         // Add Featured Image
-        $media = \media_sideload_image($movie->PosterUrl, $post_id);
-        
-        if(!empty($media) && !\is_wp_error($media)){
-            $args = array(
-                'post_type' => 'attachment',
-                'posts_per_page' => -1,
-                'post_status' => 'any',
-                'post_parent' => $post_id
-            );
-            
-            // reference new image to set as featured
-            $attachments = \get_posts($args);
-        
-            if(isset($attachments) && is_array($attachments)){
-                foreach($attachments as $attachment){
-                    // grab source of full size images (so no 300x150 nonsense in path)
-                    $image = \wp_get_attachment_image_src($attachment->ID, 'full');
-                    // determine if in the $media image we created, the string of the URL exists
-                    if(strpos($media, $image[0]) !== false){
-                        // if so, we found our image. set it as thumbnail
-                        \set_post_thumbnail($post_id, $attachment->ID);
-                        // only want one image
-                        break;
-                    }
-                }
-            }
-        }
+        $media = \media_sideload_image($movie->PosterUrl, $post_id, $movie->Name, 'id');
+        \error_log('media: ' . $media);    
     }
     
 });
